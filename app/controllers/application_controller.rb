76e733ec4ed906bbb95b6dee2083001195825db4
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2024-12-14 22:36:59 +0545

    Update the configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-  set_current_tenant_through_filter 
-  # prepend_before_action :set_current_tenant #or can use this
+  # Tells Acts as Tenant to use a before filter for setting the current tenant
+  set_current_tenant_through_filter
+  
+  # Ensures the set_current_tenant method runs before any controller action
+  prepend_before_action :set_current_tenant
 
   private
 
--- config/initializers/acts_as_tenant.rb
+++ config/initializers/acts_as_tenant.rb
@@ -1,5 +1,4 @@
 ActsAsTenant.configure do |config|
-  config.require_tenant = false # tenant lookup in application controller will be nil and give full DB access if set to false ; recommended is true
+  config.require_tenant = true # tenant lookup in application controller will be nil and give full DB access if set to false ; recommended is true
   # see other config options here: https://github.com/ErwinM/acts_as_tenant
-  # config.fallback_to_primary = true
 end
